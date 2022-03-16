9967c63ca33a03e2ba8a85abdafd57071ce78057
Ronal <66176445+Spiritbe4r@users.noreply.github.com>
2022-03-16 09:30:46 -0500

    first commit

---
+++ src/main/java/com/bootcamp/rest/BankCreditcardApplication.java
@@ -0,0 +1,13 @@
+package com.bootcamp.rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankCreditcardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankCreditcardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bootcamp/rest/BankCreditcardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bootcamp.rest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankCreditcardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
