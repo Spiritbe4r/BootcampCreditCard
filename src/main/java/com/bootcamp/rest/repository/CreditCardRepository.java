1650b1ed404fe0c1eab87ac3f3dae8f8cfdb2d5e
Ronal <66176445+Spiritbe4r@users.noreply.github.com>
2022-03-25 14:05:09 -0500

    lc

--- src/main/java/com/bootcamp/rest/repository/CreditCardRepository.java
+++ src/main/java/com/bootcamp/rest/repository/CreditCardRepository.java
@@ -4,9 +4,9 @@ import com.bootcamp.rest.models.entities.CreditCard;
 import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
 import reactor.core.publisher.Mono;
 
-public interface CreditCardRepository extends ReactiveMongoRepository<CreditCard,String> {
+public interface CreditCardRepository extends ReactiveMongoRepository<CreditCard, String> {
 
-    Mono<CreditCard> findByPan (String pan);
+  Mono<CreditCard> findByPan(String pan);
 
-    Mono<CreditCard> findByClient_ClientIdNumber(String clientIdNumber);
+  Mono<CreditCard> findByClient_ClientIdNumber(String clientIdNumber);
 }
--- src/main/java/com/bootcamp/rest/services/CreditCardService.java
+++ src/main/java/com/bootcamp/rest/services/CreditCardService.java
@@ -1,21 +1,21 @@
 package com.bootcamp.rest.services;
 
-import com.bootcamp.rest.models.entities.CreditCard;
 import com.bootcamp.rest.models.dto.Client;
 import com.bootcamp.rest.models.dto.ClientDTO;
+import com.bootcamp.rest.models.entities.CreditCard;
 import reactor.core.publisher.Mono;
 
-public interface CreditCardService extends CrudService <CreditCard,String> {
+public interface CreditCardService extends CrudService<CreditCard, String> {
 
-    public Mono<Client> getClient(String clientIdNumber);
-
-
-    public Mono<CreditCard> findByPan(String pan);
-
-    public Mono<ClientDTO> newPan(String id, ClientDTO clientDTO);
+  public Mono<Client> getClient(String clientIdNumber);
 
 
-    Mono<CreditCard> validateClientIdNumber(String clientIdNumber);
+  public Mono<CreditCard> findByPan(String pan);
 
-    public Mono<CreditCard> findByClientIdNumber(String clientIdNumber);
+  public Mono<ClientDTO> newPan(String id, ClientDTO clientDto);
+
+
+  Mono<CreditCard> validateClientIdNumber(String clientIdNumber);
+
+  public Mono<CreditCard> findByClientIdNumber(String clientIdNumber);
 }
